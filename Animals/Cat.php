9ec1c43b9b7c373695261ed41d4c2383a0613e39
Mark Rutherford <mark.rutherford@golinks.io>
2023-08-13 11:31:25 -0400

    Add documentation and break up getAnimal and generateMessage

--- Animals/Cat.php
+++ Animals/Cat.php
@@ -2,6 +2,9 @@
 
 require_once __DIR__.'/Animal.php';
 
+/*
+ * A dog class that says 'Woof!'
+ */
 class Cat extends Animal {
     public function __construct(string $name) {
         $this->sound = 'meow';
--- main.php
+++ main.php
@@ -1,22 +1,46 @@
 <?php
 
 require_once 'Animals/Cat.php';
+require_once 'Animals/Dog.php';
 
-function generateMessage($animalType, $name) {
-    switch($animalType) {
-        case 'cat':
-            $animal = new Cat($name);
-            break;
-        case 'dog':
-            $animal = new Dog($name);
-            break;
-        default:
-            return "Unknown animal type!";
-    }
-    return $animal->getName() . " says '" . $animal->getSound() . "'";
+/**
+ * @param string $animalType The type of animal
+ * @param string $name       The name of the animal
+ *
+ * @return Animal The animal object
+ * @throws Exception Unknown animal type
+ */
+function getAnimal(string $animalType, string $name): Animal {
+    return match ($animalType) {
+        'cat' => new Cat($name),
+        'dog' => new Dog($name),
+        default => throw new Exception('Unknown animal type'),
+    };
 }
 
+/**
+ * @param string $animalType The type of animal
+ * @param string $name       The name of the animal
+ *
+ * @return string The animal speaking string
+ */
+function generateMessage(string $animalType, string $name): string {
+    // Get the animal object
+    try {
+        $animal = getAnimal($animalType, $name);
+    } catch (Exception) {
+        return "Unknown animal type\n";
+    }
+
+    // Build the return string
+    $animalName = $animal->getName();
+    $animalSound = $animal->getSound();
+    return "$animalName says '$animalSound'\n";
+}
+
+// Retrieve CLI arguments
 $name = $argv[1];
 $animalType = $argv[2];
 
+// Echo the animal speaking string
 echo generateMessage($animalType, $name);
